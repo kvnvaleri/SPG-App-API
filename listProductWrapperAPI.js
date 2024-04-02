ed946e337b3384a8bf81c17dae9ec900fc8dbd27
Kevin Valeri <109036787+kvnvaleri@users.noreply.github.com>
2024-04-02 08:59:33 +0700

    Update listProductWrapperAPI.js
    New

--- listProductWrapperAPI.js
+++ listProductWrapperAPI.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const https = require('https');
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 
